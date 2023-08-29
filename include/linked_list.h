4d288e4c84b7fade0fb53253ad6983da58e202b1
SD135642 <123038434+SD135642@users.noreply.github.com>
2023-08-29 12:01:49 -0400

    Update linked_list.h

--- include/linked_list.h
+++ include/linked_list.h
@@ -1,1 +1,9 @@
-hh
+
+void print_list(linked_list *list, void (*print_function)(void*));
+
+linked_list* create_linked_list(void);
+
+bool insert_in_order(linked_list *list, void *data, int (*cmp)(const void*, const void*));
+
+void free_list(linked_list *list, void (*free_data)(void *));
+
