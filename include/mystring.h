b5fa219869b08349d9bb8fb97f5ccb049b683b32
SD135642 <123038434+SD135642@users.noreply.github.com>
2023-08-29 11:45:55 -0400

    Create mystring.h

---
+++ include/mystring.h
@@ -0,0 +1,9 @@
+
+//returns the length of the string passed
+size_t my_strlen(char *src);
+
+//copies the string and returns a pointer to the copy
+char *my_strcpy(char *dst, char *src);
+
+//makes all the characters in the string lowercase
+void my_str_lower(char *src);
